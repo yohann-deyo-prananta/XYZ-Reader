eb78d1600e6b3e3d6c5cc985e08013d136a2eb15
deyo <yohann.deyo.prananta@gmail.com>
2018-01-09 10:33:30 +0700

    Initial Commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightImageView.java
@@ -0,0 +1,37 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by deyo on 1/6/18.
+ */
+
+public class DynamicHeightImageView extends AppCompatImageView {
+    private float mAspectRatio = 1.5f;
+
+    public DynamicHeightImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+}
